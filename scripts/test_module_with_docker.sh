e7d16be318f0994e29b613d220bb52754e04892a
Jesse Reynolds <jesse@va.com.au>
2020-05-22 01:14:17 +1000

    fix path to scripts

--- scripts/test_module_with_docker.sh
+++ scripts/test_module_with_docker.sh
@@ -4,5 +4,5 @@ docker run --rm -w /test_subject \
   --mount type=bind,source="$(pwd)",target=/test_subject \
   -e LANG=C.UTF-8 \
   puppet_module_testing:latest \
-  bin/tests.sh
+  scripts/tests.sh
 
