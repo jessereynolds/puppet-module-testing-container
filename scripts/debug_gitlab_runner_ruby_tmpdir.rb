4c38dc013a1711ce3a934aca1e5a2753fb824901
Jesse Reynolds <jesse@va.com.au>
2020-05-22 10:49:21 +1000

    pull ruby tmpdir debug commands out to separate script

---
+++ scripts/debug_gitlab_runner_ruby_tmpdir.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+
+require "tmpdir"
+dir = File.expand_path("/tmp")
+puts dir.inspect
+stat = File.stat(dir)
+puts "directory? #{stat.directory?}"
+puts "writable? #{stat.writable?}"
+puts "world_writable? #{stat.world_writable?}"
+puts "sticky? #{stat.sticky?}"
+puts Dir.tmpdir
+puts Dir.mktmpdir
+
